318f8a17b08d508967f420d5ff3516908219ad91
Sakshi <sakshinigam2703@gmail.com>
2019-06-20 13:51:13 +0530

    added common styling to style.css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { GithubComponent } from './components/github/github.component';
 import { HttpClientModule } from '@angular/common/http';
 
 import {FormsModule} from '@angular/forms';
+import { GithubComponent } from './components/github/github.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     GithubComponent
-  ],
+    ],
   imports: [
     BrowserModule,
     AppRoutingModule,
